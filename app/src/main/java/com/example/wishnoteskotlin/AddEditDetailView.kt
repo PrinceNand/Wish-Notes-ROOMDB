e1cde71c27c45102a03d52d1d6f539bb1130a19c
Prince <ashnand2000@gmail.com>
2024-10-15 20:13:29 +0530

    Wish-Notes: 10.Created Add Edit Details Screen And Added dynamic OutlineTextField

---
+++ app/src/main/java/com/example/wishnoteskotlin/AddEditDetailView.kt
@@ -0,0 +1,76 @@
+package com.example.wishnoteskotlin
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun AddEditDetailView(id: Long = 0L, viewModel: WishViewModel, navController: NavController) {
+
+    Scaffold(topBar = {AppBarView(title = if (id != 0L) "Update Wish" else "Add Wish", onBackClicked = {})}) {
+        Column(modifier = Modifier
+            .padding(it)
+            .wrapContentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Spacer(modifier = Modifier.height(10.dp))
+
+
+        }
+    }
+}
+
+@Composable
+fun WishTextField(
+    label: String,
+    value: String,
+    onValueChanged: (String) -> Unit
+){
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChanged,
+        label = { Text(text = label, color = Color.Black) },
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            // using predefined Color
+            textColor = Color.Black,
+            // using our own colors in Res.Values.Color
+            focusedBorderColor = colorResource(id = R.color.black),
+            unfocusedBorderColor = colorResource(id = R.color.black),
+            cursorColor = colorResource(id = R.color.black),
+            focusedLabelColor = colorResource(id = R.color.black),
+            unfocusedLabelColor = colorResource(id = R.color.black),
+        )
+
+
+    )
+}
+
+@Preview
+@Composable
+fun WishTestFieldPrev(){
+    WishTextField(label = "text", value = "text", onValueChanged = {})
+}
+
