7545ac85a4685b1a975492e4d7d5099712e98bc6
Prince <ashnand2000@gmail.com>
2024-10-16 12:55:20 +0530

    Wish-Notes: 19.Add A Wish in DB and show a snack bar

--- app/src/main/java/com/example/wishnoteskotlin/AddEditDetailView.kt
+++ app/src/main/java/com/example/wishnoteskotlin/AddEditDetailView.kt
@@ -13,12 +13,16 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,12 +34,22 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.wishnoteskotlin.data.Wish
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AddEditDetailView(id: Long = 0L, viewModel: WishViewModel, navController: NavController) {
 
-    Scaffold(topBar = {AppBarView(title = if (id != 0L) "Update Wish" else "Add Wish", onBackClicked = {navController.navigateUp()})}) {
+    val snackMessage = remember { mutableStateOf("") }
+    // Creates a coroutine scope that is tied to the lifecycle of the composable, allowing you to launch coroutines safely within it.
+    val scope = rememberCoroutineScope()
+
+    // Creates and remembers the state of a Scaffold, which is used to control UI elements like snackbars, bottom sheets, or the drawer.
+    val scaffoldState = rememberScaffoldState()
+
+
+    Scaffold(scaffoldState = scaffoldState,topBar = {AppBarView(title = if (id != 0L) "Update Wish" else "Add Wish", onBackClicked = {navController.navigateUp()})}) {
         Column(modifier = Modifier
             .padding(it)
             .wrapContentSize(),
@@ -62,10 +76,26 @@ fun AddEditDetailView(id: Long = 0L, viewModel: WishViewModel, navController: Na
                 if(viewModel.wishTitleState.isNotEmpty() &&
                     viewModel.wishDescriptionState.isNotEmpty()){
                     //TODO UpdateWish
-                }else{
                     // TODO AddWish
+                    if (id!=0L){
+                        // Update Wish
+                    } else {
+                        // Add Wish
+                        viewModel.addWish(
+                            Wish(
+                                title = viewModel.wishTitleState.trim(),
+                                description = viewModel.wishDescriptionState.trim()
+                            )
+                        )
+                    }
+                    snackMessage.value = "Wish has been created"
+                }else{
+                    snackMessage.value = "Enter Feild to add Wish"
                 }
-
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(snackMessage.value)
+                    navController.navigateUp()
+                }
 
             }){
                 Text(
